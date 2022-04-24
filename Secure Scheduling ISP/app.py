6995a301c420b40ef65d3e13b0e0e012d29d2951
prem-kishnani <prem-kishnani@student.allenisd.org>
2022-04-24 16:45:32 -0500

    Added checklist, also fixed some scheduling stuff

--- Secure Scheduling ISP/app.py
+++ Secure Scheduling ISP/app.py
@@ -3,24 +3,65 @@ from my_project import app,db
 from flask import render_template, redirect, request, url_for, flash, abort
 from flask_login import login_user, login_required, logout_user, current_user
 from my_project.models import User
-from my_project.forms import LoginForm, RegistrationForm
+from my_project.forms import LoginForm, RegistrationForm, CourseForm
 from werkzeug.security import generate_password_hash, check_password_hash
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('schedules.csv')
-df.set_index('Email',inplace=True)
 
-@app.route('/home')
+
+@app.route('/home', methods=['GET','POST'])
 @login_required
-def home():
+def home(): 
+    df = pd.read_csv('schedules.csv',nrows=100000)
+    df.set_index('Email',inplace=True)
+
+    form = CourseForm()
+
+
+
+    if form.validate_on_submit():
+        print('Validated Form')
+        print(form.p1.data)
+        print(current_user.email)
+        #1. Read values and store them in our dataframe
+        df.at[current_user.email,"Period 1"] = form.p1.data
+        df.at[current_user.email,"Period 2"] = form.p2.data
+        df.at[current_user.email,"Period 3"] = form.p3.data
+        df.at[current_user.email,"Period 4"] = form.p4.data
+        df.at[current_user.email,"Period 5"] = form.p5.data
+        df.at[current_user.email,"Period 6"] = form.p6.data
+        df.at[current_user.email,"Period 7"] = form.p7.data
+        df.at[current_user.email,"Period 8"] = form.p8.data
+        print(df.loc[current_user.email])
+
+        #2. Overwrite the csv file
+        df.to_csv("schedules.csv") 
+
+        return redirect(url_for('updated'))
+
     if current_user.is_authenticated:
+
+        # If email not in DF index then add a row with that user and blank values for Periods in DF
+        if current_user.email not in df.index:
+            df.at[current_user.email] = None
+
         schedule = []
         print(current_user.email)
         for i in range(1,9):
             class_name = df.loc[current_user.email,f"Period {i}"]
             schedule.append(class_name)
-    return render_template('home.html', schedule=schedule)
+        form.p1.data = schedule[0]
+        form.p2.data = schedule[1]
+        form.p3.data = schedule[2]
+        form.p4.data = schedule[3]
+        form.p5.data = schedule[4]
+        form.p6.data = schedule[5]
+        form.p7.data = schedule[6]
+        form.p8.data = schedule[7]
+
+
+    return render_template('home.html', form=form)
     
 @app.route('/welcome')
 @login_required
@@ -70,6 +111,10 @@ def register():
         return redirect(url_for('login'))
     
     return render_template('register.html', form=form)
+
+@app.route('/updated')
+def updated():
+    return render_template("db_updated.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Secure Scheduling ISP/my_project/forms.py
+++ Secure Scheduling ISP/my_project/forms.py
@@ -1,7 +1,7 @@
 #forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired,Email,EqualTo, InputRequired, Length
 from wtforms import ValidationError
 
 class LoginForm(FlaskForm):
@@ -15,6 +15,17 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm', message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
     submit = SubmitField('Register!')
+
+class CourseForm(FlaskForm):
+    p1 = TextAreaField("1st Period",validators=[DataRequired(),Length(max=30)])
+    p2 = TextAreaField("2nd Period",validators=[DataRequired(),Length(max=30)])
+    p3 = TextAreaField("3rd Period",validators=[DataRequired(),Length(max=30)])
+    p4 = TextAreaField("4th Period",validators=[DataRequired(),Length(max=30)])
+    p5 = TextAreaField("5th Period",validators=[DataRequired(),Length(max=30)])
+    p6 = TextAreaField("6th Period",validators=[DataRequired(),Length(max=30)])
+    p7 = TextAreaField("7th Period",validators=[DataRequired(),Length(max=30)])
+    p8 = TextAreaField("8th Period",validators=[DataRequired(),Length(max=30)])
+    save = SubmitField("Save!")
 
 def check_email(self,field):
     if User.query.filter_by(email=field.data).first():
